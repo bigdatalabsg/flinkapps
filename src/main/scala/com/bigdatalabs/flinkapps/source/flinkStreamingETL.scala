22e63b5373c8e369ecdea6ba6e032b865a958d0b
bigdatalabsg <bigdatalabsg@gmail.com>
2022-04-02 18:21:43 +0800

    New File

--- src/main/scala/com/bigdatalabs/flinkapps/source/flinkStreamingETL.scala
+++ src/main/scala/com/bigdatalabs/flinkapps/source/flinkStreamingETL.scala
@@ -1,5 +1,13 @@
 package com.bigdatalabs.flinkapps.source
 
 object flinkStreamingETL {
-
+  def main(args: Array[String]): Unit = {
+ if (args.length < 2) {
+      System.err.println(
+        s"""
+           |Usage: --topic_source <source topic name> --topic_sink <sink topic name> --groupId <group name> --high <number> --low <number>
+        """.stripMargin)
+      System.exit(1)
+    }
+  }
 }
