6d3df38ed56537c2f14a4335b0615a3b45f686ca
bigdatalabsg <bigdatalabsg@gmail.com>
2022-04-10 23:36:52 +0800

    Code Snippets for Table API

--- src/main/scala/com/bigdatalabs/flinkapps/source/flinkStreamTable.scala
+++ src/main/scala/com/bigdatalabs/flinkapps/source/flinkStreamTable.scala
@@ -7,8 +7,9 @@ package com.bigdatalabs.flinkapps.source
 
  */
 
+import org.apache.flink.api.common.RuntimeExecutionMode
+import org.apache.flink.api.scala._
 import org.apache.flink.api.java.utils.ParameterTool
-
 import org.apache.flink.api.scala.typeutils.Types
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.table.api.bridge.scala.StreamTableEnvironment
@@ -52,9 +53,60 @@ object flinkStreamTable {
     )
 
     // create environments of both APIs
-    val _env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
-    val _tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(_env)
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val tableEnv: StreamTableEnvironment = StreamTableEnvironment.create(env)
 
+    // set the batch runtime mode
+    env.setRuntimeMode(RuntimeExecutionMode.BATCH)
+
+    // create a DataStream
+    val dataStream = env.fromElements(
+      Row.of("Alice", Int.box(12)),
+      Row.of("Bob", Int.box(10)),
+      Row.of("Alice", Int.box(100))
+    )(Types.ROW(Types.STRING, Types.INT))
+
+    // interpret the insert-only DataStream as a Table
+    val inputTable = tableEnv.fromDataStream(dataStream).as("name", "score")
+
+    // register the Table object as a view and query it
+    // the query contains an aggregation that produces updates
+    tableEnv.createTemporaryView("InputTable", inputTable)
+    val resultTable = tableEnv.sqlQuery("SELECT name, SUM(score) FROM InputTable GROUP BY name")
+
+    // interpret the updating Table as a changelog DataStream
+    val resultStream = tableEnv.toChangelogStream(resultTable)
+
+    // add a printing sink and execute in DataStream API
+    resultStream.print()
+    env.execute()
+
+    // create a DataStream
+    //val dataStream = env.fromElements("Alice", "Bob", "John")
+
+    /*
+    val dataStream = env.fromElements(
+      Row.of("Alice", Int.box(12)),
+      Row.of("Bob", Int.box(10)),
+      Row.of("Alice", Int.box(100))
+    )(Types.ROW(Types.STRING, Types.INT))
+
+    // interpret the insert-only DataStream as a Table
+    val inputTable = tableEnv.fromDataStream(dataStream)
+
+    // register the Table object as a view and query it
+    tableEnv.createTemporaryView("InputTable", inputTable)
+    val resultTable = tableEnv.sqlQuery("SELECT * FROM InputTable")
+
+    // interpret the insert-only Table as a DataStream again
+    val resultStream = tableEnv.toDataStream(resultTable)
+
+    // add a printing sink and execute in DataStream API
+    resultStream.print()
+    env.execute()
+    */
+
+    /*
     // create a DataStream
     val _dataStream = _env.fromElements(
       Row.of("Alice", Int.box(12)),
@@ -80,6 +132,12 @@ object flinkStreamTable {
     _resultStream.print()
 
     _env.execute()
+
+    */
+
+
+
+
 
   }
 
